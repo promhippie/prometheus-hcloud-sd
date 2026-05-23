9d9f7d9bee6d54cae2e51a266e16e490add004c4
Thomas Boerger <thomas@webhippie.de>
2026-05-23 10:44:39 +0200

    feat: upgrade required go version to 1.26

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
                 languages = {
                   go = {
                     enable = true;
-                    package = pkgs.go_1_25;
+                    package = pkgs.go_1_26;
                   };
                 };
 
